181adb7fc6a0f1ac52e53b0214a7f5875064997c
Sofia Shulyak <s.shulyak@innopolis.university>
2023-07-03 17:36:25 +0300

    mode-constrained random melody generation + rhythm fixed

--- assign1.py
+++ assign1.py
@@ -4,7 +4,8 @@ from pychord import Chord
 from random import randint,shuffle
 from math import ceil
 
-TICKS_PER_BAR = 1536
+NUM_OF_BARS = 32
+TICKS_PER_BAR = 1920
 MODES = [
     [2, 2, 1, 2, 2, 2, 1], # IONIAN
     [2, 1, 2, 2, 2, 1, 2], # DORIAN
@@ -12,7 +13,7 @@ MODES = [
     [2, 2, 2, 1, 2, 2, 1], # LYDIAN
     [2, 2, 1, 2, 2, 1, 2], # MIXOLYDIAN
     [2, 1, 2, 2, 1, 2, 2], # AEOLIAN
-    [1, 2, 2, 1, 2, 2, 2] # LOCRIAN
+    [1, 2, 2, 1, 2, 2, 2], # LOCRIAN
 ]
 NUMBER_TO_NOTE = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 NOTE_TO_NUMBER = {
@@ -32,7 +33,7 @@ NOTE_TO_NUMBER = {
 
 # args: [[1]..[1]] with len=num of bars
 def subdivide(bars: list[list[int]]) -> list[list[int]]:
-    if bars[0][0]==16:
+    if bars[0][0]==8:
         return bars
     divide = randint(0,1)
     if divide:
@@ -50,15 +51,20 @@ melody_track = [
 ]
 out_file = MidiFile()
 
-#key = randint(0,11)
-#mode = randint(0,6)
-subdivision = subdivide([[1] for _ in range(32)])
+key = randint(0,11)
+mode = randint(0,6)
+octave = randint(-1,4)
+print(NUMBER_TO_NOTE[key],mode,octave)
+
+allowed_notes = [key]
+for interval in MODES[mode]:
+    key += interval
+    allowed_notes.append(key)
+
+subdivision = subdivide([[1] for _ in range(NUM_OF_BARS)])
 for bar in subdivision:
     for denominator in bar:
-        octave = randint(-1,4)
-        note = randint(0,11)
-        print(NUMBER_TO_NOTE[note],octave)
-        note += 36 + 12*octave
+        note = allowed_notes[randint(0,6)] + 36 + 12*octave
         melody_track.append(Message("note_on", note=note, velocity=50, time=0))
         melody_track.append(Message("note_off", note=note, velocity=50, time=ceil(TICKS_PER_BAR/denominator)))
 
